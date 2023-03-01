7cadff5dfd894f9533551279213d8b7145e82cb6
DATAUNIRIO <duttross@gmail.com>
2023-03-01 16:42:24 -0300

    Update limpeza_dos_dados_do_perfil.R

--- R/codigo/limpeza_dos_dados_do_perfil.R
+++ R/codigo/limpeza_dos_dados_do_perfil.R
@@ -1,3 +1,4 @@
+library(janitor)
 library(readxl)
 alunos <- read_excel("C:/Users/Hp/Desktop/Borba Mobiliza Rio/dados/Perfil alunos mobiliza rio março de 23(1).xlsx", 
                      , 
@@ -21,76 +22,75 @@ alunos <- read_excel("C:/Users/Hp/Desktop/Borba Mobiliza Rio/dados/Perfil alunos
                                    "text", "text", "text", "text", "text", 
                                    "text", "text", "text", "text", "text", 
                                    "numeric", "numeric"),
-                     sheet = "alunos(1)")
+                     sheet = "alunos(1)") %>% clean_names()
 names(alunos)
 
 alunos = alunos[,c(1,2,5,7,8,27:34,83:87)]
 alunos = alunos[-1,]
 
-alunos$NASCIMENTO[2]
+alunos$nascimento[2]
 #as.POSIXct.Date(Sys.Date())-alunos$NASCIMENTO[2]
 
-trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$NASCIMENTO[2], units = "days")) / 365.25)
-alunos$idade = trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$NASCIMENTO, units = "days")) / 365.25)
+trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$nascimento[2], units = "days")) / 365.25)
+alunos$idade = trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$nascimento, units = "days")) / 365.25)
 max(alunos$idade, na.rm = TRUE)
 min(alunos$idade, na.rm = TRUE)
 
 alunos$idade = ifelse(alunos$idade<1,NA,alunos$idade)
 
 library(stringr)
-alunos$BAIRRO = str_to_title(alunos$BAIRRO)
-table(alunos$BAIRRO)
+alunos$bairro = str_to_title(alunos$bairro)
+table(alunos$bairro)
 
 # Freguesia,Freguesia Jacarepaguá, Freguesia Jacarépagua
-# MELHOR QUE O IF_ELSE
- 
-alunos$BAIRRO = gsub('Tijuca 20530420','Tijuca',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Praca Da Bandeira', 'Praça Da Bandeira',alunos$BAIRRO)
 
-alunos$BAIRRO = gsub('Recreio Dos Bandeitantes','Recreio Dos Bandeirantes',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Recreio Bandeirantes','Recreio Dos Bandeirantes',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Recreio Dos Bandeirantes','Recreio',alunos$BAIRRO)
+alunos$bairro = gsub('Tijuca 20530420','Tijuca',alunos$bairro)
+alunos$bairro = gsub('Praca Da Bandeira', 'Praça Da Bandeira',alunos$bairro)
 
-alunos$BAIRRO = gsub('Realengp','Realengo',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Realengo Barata','Realengo',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Relengo','Realengo',alunos$BAIRRO)
+alunos$bairro = gsub('Recreio Dos Bandeitantes','Recreio Dos Bandeirantes',alunos$bairro)
+alunos$bairro = gsub('Recreio Bandeirantes','Recreio Dos Bandeirantes',alunos$bairro)
+alunos$bairro = gsub('Recreio Dos Bandeirantes','Recreio',alunos$bairro)
 
-alunos$BAIRRO = gsub('Quintino Bocaiúva','Quintino',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Quintino Bocaiuva','Quintino',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Quintino','Quintino Bocaiúva',alunos$BAIRRO)
+alunos$bairro = gsub('Realengp','Realengo',alunos$bairro)
+alunos$bairro = gsub('Realengo Barata','Realengo',alunos$bairro)
+alunos$bairro = gsub('Relengo','Realengo',alunos$bairro)
+
+alunos$bairro = gsub('Quintino Bocaiúva','Quintino',alunos$bairro)
+alunos$bairro = gsub('Quintino Bocaiuva','Quintino',alunos$bairro)
+alunos$bairro = gsub('Quintino','Quintino Bocaiúva',alunos$bairro)
+
+alunos$bairro = gsub('Pedra De Guaratiba Piraquê','Pedra De Guaratiba',alunos$bairro) 
+alunos$bairro = gsub('Piraque Pedra De Guaratiba','Pedra De Guaratiba',alunos$bairro)          
+alunos$bairro = gsub('Piraque','Pedra De Guaratiba',alunos$bairro) 
+
+alunos[as.numeric(which(alunos$bairro == "Maré")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Mare")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Maré Bonsucesso")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Bonsucesso/ Maré")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Bonsucesso")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Vila Do Pinheiro, Maré")),'bairro'] = 'Bonsucesso/Maré'
 
 
-alunos$BAIRRO = gsub('Pedra De Guaratiba Piraquê','Pedra De Guaratiba',alunos$BAIRRO) 
-alunos$BAIRRO = gsub('Piraque Pedra De Guaratiba','Pedra De Guaratiba',alunos$BAIRRO)          
-alunos$BAIRRO = gsub('Piraque','Pedra De Guaratiba',alunos$BAIRRO) 
+alunos[as.numeric(which(alunos$bairro == "Freguesia Jacarépagua")),'bairro'] = 'Freguesia/Jacarepaguá'
+alunos[as.numeric(which(alunos$bairro == "Freguesia Jacarepaguá")),'bairro'] = 'Freguesia/Jacarepaguá'
+alunos[as.numeric(which(alunos$bairro == "Jacarepaguá")),'bairro'] = 'Freguesia/Jacarepaguá'
+alunos[as.numeric(which(alunos$bairro == "Freguesia")),'bairro'] = 'Freguesia/Jacarepaguá'
+alunos[as.numeric(which(alunos$bairro == "Manguinho")),'bairro'] = 'Manguinhos'
+alunos[as.numeric(which(alunos$bairro == "Cjt Nova Sepetiba")),'bairro'] = 'Nova Sepetiba'
+alunos[as.numeric(which(alunos$bairro == "Santa Cruz Areia Branca")),'bairro'] = 'Santa Cruz'
 
-alunos[as.numeric(which(alunos$BAIRRO == "Maré")),'BAIRRO'] = 'Bonsucesso/Maré'
-alunos[as.numeric(which(alunos$BAIRRO == "Mare")),'BAIRRO'] = 'Bonsucesso/Maré'
-alunos[as.numeric(which(alunos$BAIRRO == "Maré Bonsucesso")),'BAIRRO'] = 'Bonsucesso/Maré'
-alunos[as.numeric(which(alunos$BAIRRO == "Bonsucesso/ Maré")),'BAIRRO'] = 'Bonsucesso/Maré'
-alunos[as.numeric(which(alunos$BAIRRO == "Bonsucesso")),'BAIRRO'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Vila Do Joao")),'bairro'] = 'Vila Do João'
 
-alunos[as.numeric(which(alunos$BAIRRO == "Freguesia Jacarépagua")),'BAIRRO'] = 'Freguesia/Jacarepaguá'
-alunos[as.numeric(which(alunos$BAIRRO == "Freguesia Jacarepaguá")),'BAIRRO'] = 'Freguesia/Jacarepaguá'
-alunos[as.numeric(which(alunos$BAIRRO == "Jacarepaguá")),'BAIRRO'] = 'Freguesia/Jacarepaguá'
-alunos[as.numeric(which(alunos$BAIRRO == "Freguesia")),'BAIRRO'] = 'Freguesia/Jacarepaguá'
+            
 
-table(alunos$BAIRRO)
+table(alunos$bairro)
+
 
 # http://homepage.stat.uiowa.edu/~luke/classes/STAT4580-2022/proportions.html
 
-alunos$BAIRRO = gsub('[Freguesia Jacarepaguá]','',alunos$BAIRRO)
-alunos$BAIRRO = gsub('[Freguesia Jacarépagua]','Freguesia/Jacarepaguá',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Freguesia','Freguesia/Jacarepaguá',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Jacarepaguá','Freguesia/Jacarepaguá',alunos$BAIRRO)
-
-
-Freguesia
-
- 
-Jacarepaguá
 
 
 
-alunos$BAIRRO = gsub('Freguesia/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$BAIRRO)
-alunos$BAIRRO = gsub('Freguesia/Freguesia/Jacarepaguá/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$BAIRRO)
+
+alunos$bairro = gsub('Freguesia/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$bairro)
+alunos$bairro = gsub('Freguesia/Freguesia/Jacarepaguá/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$bairro)
