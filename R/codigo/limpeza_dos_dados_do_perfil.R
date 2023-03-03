8f4fa3dfabf4e23e493158eac2f3714dafa9a96b
DATAUNIRIO <duttross@gmail.com>
2023-03-03 19:50:31 -0300

    Update limpeza_dos_dados_do_perfil.R

--- R/codigo/limpeza_dos_dados_do_perfil.R
+++ R/codigo/limpeza_dos_dados_do_perfil.R
@@ -35,8 +35,48 @@ trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$nascimento[2], uni
 alunos$idade = trunc(as.numeric(difftime(as.POSIXct.Date(Sys.Date()), alunos$nascimento, units = "days")) / 365.25)
 max(alunos$idade, na.rm = TRUE)
 min(alunos$idade, na.rm = TRUE)
-
 alunos$idade = ifelse(alunos$idade<1,NA,alunos$idade)
+
+
+alunos = alunos %>%
+  mutate(
+    faixa_idade = case_when(
+      idade <= 18 ~ "01 (18 anos ou menos)",
+      idade <= 25 ~ "02 (18 à 25 anos)",
+      idade <= 30 ~ "03 (26 à 30 anos)",
+      idade <= 35 ~ "04 (31 à 35 anos)",
+      idade <= 40 ~ "05 (36 à 40 anos)",
+      idade <= 45 ~ "06 (41 à 45 anos)",
+      idade <= 50 ~ "07 (46 à 50 anos)",
+      idade <= 55 ~ "08 (51 à 55 anos)",
+      idade <= 60 ~ "09 (56 à 60 anos)",
+      idade <= 90 ~ "10 (61 anos ou mais)",
+      TRUE~ '999'))
+
+
+alunos$faixa_idade = ifelse(alunos$faixa_idade=='999',NA,alunos$faixa_idade)
+table(alunos$faixa_idade)
+
+
+alunos = alunos %>%
+  mutate(
+    faixa_idade2 = case_when(
+      idade <= 18 ~ "1",
+      idade <= 29 ~ "2",
+      idade <= 39 ~ "3",
+      idade <= 49 ~ "4",
+      idade <= 60 ~ "5",
+      idade <= 90 ~ "6",
+      TRUE~ '999'))
+table(alunos$faixa_idade2)
+alunos$faixa_idade2 = ifelse(alunos$faixa_idade2=='999',NA,alunos$faixa_idade2)
+table(alunos$faixa_idade2)
+
+library(stringr)
+#str_match(alunos$atividade_1, "Núcleo:\\s*(.*?)\\s")
+alunos$nucleo = str_match(alunos$atividade_1, "Núcleo:\\s\\d\\d")
+table(alunos$nucleo)
+alunos$nucleo = gsub('Núcleo: ','',alunos$nucleo)
 
 library(stringr)
 alunos$bairro = str_to_title(alunos$bairro)
@@ -69,8 +109,10 @@ alunos[as.numeric(which(alunos$bairro == "Maré Bonsucesso")),'bairro'] = 'Bonsu
 alunos[as.numeric(which(alunos$bairro == "Bonsucesso/ Maré")),'bairro'] = 'Bonsucesso/Maré'
 alunos[as.numeric(which(alunos$bairro == "Bonsucesso")),'bairro'] = 'Bonsucesso/Maré'
 alunos[as.numeric(which(alunos$bairro == "Vila Do Pinheiro, Maré")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Bonsucesso/ maré")),'bairro'] = 'Bonsucesso/Maré'
+alunos[as.numeric(which(alunos$bairro == "Bonsucesso/maré")),'bairro'] = 'Bonsucesso/Maré'
 
-
+                       
 alunos[as.numeric(which(alunos$bairro == "Freguesia Jacarépagua")),'bairro'] = 'Freguesia/Jacarepaguá'
 alunos[as.numeric(which(alunos$bairro == "Freguesia Jacarepaguá")),'bairro'] = 'Freguesia/Jacarepaguá'
 alunos[as.numeric(which(alunos$bairro == "Jacarepaguá")),'bairro'] = 'Freguesia/Jacarepaguá'
@@ -81,16 +123,137 @@ alunos[as.numeric(which(alunos$bairro == "Santa Cruz Areia Branca")),'bairro'] =
 
 alunos[as.numeric(which(alunos$bairro == "Vila Do Joao")),'bairro'] = 'Vila Do João'
 
-            
-
 table(alunos$bairro)
 
+alunos$bairro2 = forcats::fct_lump_min(alunos$bairro,10) 
+table(alunos$bairro2)
 
+
+  
 # http://homepage.stat.uiowa.edu/~luke/classes/STAT4580-2022/proportions.html
 
+library(dplyr)
+library(ggplot2)
+agg = alunos %>% select(bairro2,sexo) %>% table() %>% data.frame()
+agg
+
+ggplot(agg) +
+  geom_col(aes(x = bairro2, y = Freq, fill = sexo), position = "fill") +
+  scale_fill_manual(values = c('red','blue')) 
+
+mutate(agg, bairro2 = reorder(bairro2, Freq)) %>%
+  ggplot(aes(x = ifelse(sexo == "MASCULINO", Freq, -Freq),
+             y = bairro2,
+             fill = sexo)) +
+  geom_col() +
+  scale_fill_manual(values = c('red','blue')) +
+  xlab("Quantidade de participantes")+
+  ylab("Localidade")
+  
+  
+
+library(waffle)
+stopifnot(packageVersion("waffle") >= "1.0.1")
+ggplot(arrange(agg, bairro2), aes(values = Freq, fill = bairro2)) +
+  geom_waffle(n_rows = 18, flip = TRUE, color = "white", size = 0.33) +
+  coord_equal() +
+  facet_wrap(~ sexo) +
+  scale_fill_manual(values = c('#00425A','#1F8A70','#BFDB38','#FC7300','#f0bb84','gray40','gray70','gray80',
+'#F273E6','#CF4DCE','#912490')) +
+  theme_minimal() +
+  theme_enhance_waffle()
 
 
+ggplot(arrange(agg, sexo), aes(values = Freq, fill = sexo)) +
+  geom_waffle(n_rows = 18, flip = TRUE, color = "white", size = 0.33) +
+  coord_equal() +
+  facet_wrap(~ bairro2, ncol=2) +
+  scale_fill_manual(values = c('#1F8A70','#FC7300')) +
+                                          theme_minimal() +
+  theme_enhance_waffle()
 
 
-alunos$bairro = gsub('Freguesia/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$bairro)
-alunos$bairro = gsub('Freguesia/Freguesia/Jacarepaguá/Freguesia/Jacarepaguá','Freguesia/Jacarepaguá',alunos$bairro)
+group_by(alunos, bairro2,sexo) %>% mutate(percent = value/sum(value))
+
+library(dplyr)
+table(agg$sexo)
+agg %>% group_by(bairro2) %>% summarise(total=sum(Freq))
+agg %>% group_by(bairro2,sexo) %>% mutate(percent =  Freq/sum(Freq)*100)
+
+library(dplyr)
+agg = agg %>%
+  group_by(bairro2) %>%
+  mutate(countT= sum(Freq)) %>%
+  group_by(Freq, add=TRUE) %>%
+  mutate(per=100*Freq/countT,2)
+  #mutate(per=paste0(round(100*Freq/countT,2),'%'))
+
+
+ggplot(arrange(agg, sexo), aes(values = per, fill = sexo)) +
+  geom_waffle(n_rows = 20, flip = TRUE, color = "white", size = 0.33) +
+  coord_equal() +
+  facet_wrap(~ bairro2, ncol=2) +
+  scale_fill_manual(values = c('#1F8A70','#FC7300')) +
+  theme_minimal() +
+  theme_enhance_waffle()
+
+
+#----------------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+agg2 = alunos %>% select(faixa_idade,bairro2,sexo) %>% table() %>% data.frame()
+agg2
+
+  
+agg2 %>%  ggplot(aes(x = ifelse(sexo == "MASCULINO", Freq, -Freq),
+             y = faixa_idade,
+             fill = sexo)) +
+  geom_col() +
+  scale_fill_manual(values = c('red','blue')) +
+  xlab("Quantidade de participantes")+
+  ylab("IDADE")
+
+agg2 %>%  ggplot(aes(x = ifelse(sexo == "MASCULINO", Freq, -Freq),
+                     y = faixa_idade,
+                     fill = sexo)) +
+  geom_col() +
+  scale_fill_manual(values = c('red','blue')) +
+  facet_wrap(~ bairro2, ncol=2) +
+  xlab("Quantidade de participantes")+
+  ylab("IDADE")
+
+
+#----------------------------------------------------------------------------------
+
+library(dplyr)
+library(ggplot2)
+agg3 = alunos %>% select(nucleo, faixa_idade2) %>% table() %>% data.frame()
+agg3
+agg3 = agg3 %>%
+  group_by(nucleo) %>%
+  mutate(countT= sum(Freq)) %>%
+  group_by(faixa_idade2, add=TRUE) %>%
+  mutate(per=100*Freq/countT,2)
+agg3 %>% filter(nucleo=='02')
+
+ggplot()+
+  geom_bar(data = agg3, aes(x = nucleo, y=per, fill=faixa_idade2), position="stack", stat="identity")+
+  coord_flip() + 
+  ggtitle("Faixa etária por núcleo")+
+  ylab("Percentual")+
+  xlab("Núcleo")+
+  scale_fill_brewer(palette="RdBu",labels =c("18 ou menos", "18 até 29", "30 até 39","40 até 49","50 até 59", "60 ou mais"))+
+  theme(legend.position="bottom") 
+
+
+agg4 = alunos %>% select(nucleo, idade) %>% group_by(nucleo) %>% summarise(média=mean(idade, na.rm =T))
+agg4
+
+ggplot()+
+  geom_point(data = agg4, aes(x = nucleo, y=média), stat="identity", size =5, fill='blue')+
+  coord_flip() + 
+  ggtitle("Idade média por núcleo")+
+  ylab("Idade Média")+
+  xlab("Núcleo")
+
